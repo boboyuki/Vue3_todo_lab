e3b9cc333cbc73b7a3aedd9fb3254ac9d1d3d9f2
BoBo <bobonani1214@gmail.com>
2022-05-25 00:19:33 +0800

    feat: init project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import "./index.css"
+import { createPinia } from "pinia"
+const pinia = createPinia();
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.use(pinia)
+.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path")
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "./src"),
+        "@pages": path.join(__dirname, "./src/pages"),
+        "@components": path.join(__dirname, "./src/components"),
+        "@utils": path.join(__dirname, "./src/utils"),
+      },
+    },
+  },
+});
